553fb22a4badd3d8bf5a1d91e794344645476411
Olawoye Iyanuluwa <iyolawoye@gmail.com>
2024-04-28 15:16:14 +0100

    testing

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -3,13 +3,13 @@ import style from "./style.module.css";
 
 export const Modal = ({ children, isOpen, onClose }) => {
   return (
-    <>
+    <div>
       {isOpen && (
         <div className={style.modal}>
           <div onClick={onClose} className={style.overlay}></div>
           <div className={style["modal-content"]}>{children}</div>
         </div>
       )}
-    </>
+    </div>
   );
 };
